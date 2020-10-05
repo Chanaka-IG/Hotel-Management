e22209c7d696a79f4a25beef088f1c7022044b31
chanaka prasad <igchanaka.prasad@gmail.com>
2020-10-05 22:59:41 +0530

    hotel managment java file added

---
+++ src/hotel/managment/HotelManagment.java
@@ -0,0 +1,14 @@
+
+package hotel.managment;
+
+import org.nlt.view.MainFrame;
+
+public class HotelManagment {
+
+  
+    public static void main(String[] args) {
+       new MainFrame().setVisible(true);
+       
+    }
+    
+}
